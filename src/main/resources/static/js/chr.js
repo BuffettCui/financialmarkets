ecf7a8bbb9eef43fb808a9572ed59793fc48d47e
cuijianang <cuijianang@vip.qq.com>
2019-09-03 18:25:12 +0800

    #first

---
+++ src/main/resources/static/js/chr.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+});
+});
+$(function() {
+    var pull 		= $('#pull');
+        menu 		= $('nav ul');
+        menuHeight	= menu.height();
+
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
